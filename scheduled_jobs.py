a6cfe2fbca35529b65155c1bb43442726a9073dd
Tosimi <tosimiadesoye111@gmail.com>
2026-04-07 13:55:06 +0100

    update date to local

--- scheduled_jobs.py
+++ scheduled_jobs.py
@@ -29,7 +29,7 @@ def process_orders(app):
         payload = {
             "product": order.product,
             "customer": order.customer,
-            "date": order.date_placed.isoformat(),
+            "date": order.date_placed_local.isoformat(),
         }
 
         response = requests.post(
