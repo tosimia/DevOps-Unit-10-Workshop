757cc95b531fb67e0799db0b57fb0cbc21a05c85
Tosimi <tosimiadesoye111@gmail.com>
2026-04-07 14:51:55 +0100

    add live metrics

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from azure.monitor.opentelemetry import configure_azure_monitor
 import logging
 
 logging.basicConfig(level=logging.INFO)
-configure_azure_monitor()
+configure_azure_monitor(enable_live_metrics=True)
 
 from flask import Flask, render_template, request
 from datetime import datetime, timezone
